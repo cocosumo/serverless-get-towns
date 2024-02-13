11de2cddcc75482f080865a9e730458187a8a2b1
Lorenz Ras <lenzras@gmail.com>
2024-02-13 14:32:15 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 
+console.log('BASE_URL', process.env.BASE_URL);
+
 const kintoneClient = new KintoneRestAPIClient({
   baseUrl: process.env.BASE_URL,
   // Use API token authentication
   auth: { apiToken: process.env.API_TOKEN }
 });
+
+console.log('CREATED!');
 
 
 export default kintoneClient;
