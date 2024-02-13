5a8397cbe5cf6513d9c8e1e9e47d997a22235192
Lorenz Ras <lenzras@gmail.com>
2024-02-13 12:32:36 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import getTowns from './src/getTowns';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  await getTowns();
+  
   return {
       statusCode: 200,
       body: JSON.stringify({
