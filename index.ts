8ebb1d630c983dd903d9ae3edabe8e103b4ee2f9
Lorenz Ras <lenzras@gmail.com>
2024-02-13 16:47:49 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -26,5 +26,10 @@ export const handler = async (
         body: JSON.stringify({
             data: result,
         }),
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
     };
 };
