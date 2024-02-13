e96633ea8c0c2fd6ec2ee9b5e037e1c12e9a62da
Lorenz Ras <lenzras@gmail.com>
2024-02-13 17:00:31 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 import getTowns from './src/getTowns';
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    'test-site2.com',
+    'yumenootetsudai.com'
+];
+
 export const handler = async (
-    event: APIGatewayEvent,  
+    event: APIGatewayEvent, 
+    context: any
 ): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+
+    // get origin
+    const origin = event.headers.origin || event.headers.Origin || '';
+
+    console.log(`Origin: ${origin}`);
+    
+    // check if origin is allowed
+    const accessAllowOrigin = allowedOrigins.includes(origin) ? origin : allowedOrigins[0];
 
     const {
         city
@@ -28,7 +43,7 @@ export const handler = async (
         }),
         headers: {
             "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": accessAllowOrigin,
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
     };
